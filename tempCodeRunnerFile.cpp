1cf48b48dae5dbafaa1337ff1cf347a8fce063e9
SHIVA CHARAN THUDI <103764197+shivacharan05122003apple@users.noreply.github.com>
2023-09-18 20:33:14 +0530

    new cpp

---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a;//declaration
+    a=11;//initialization
+
+    cout<<"size of int "<<sizeof(a)<<endl;
+
+    float b;
+    cout<<"size of float "<<sizeof(b)<<endl;
+     
+    char c;
+    cout<<"size of char "<<sizeof(c)<<endl;
+    bool d;
+    cout<<"size of bool "<<sizeof(d)<<endl;
+    
+
+}
